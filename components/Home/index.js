30479d87d8b71e9db4f8b4bd59b7d9cedd9e929c
tansang106 <tansang106@gmail.com>
2018-11-19 11:07:34 +0700

    Login connect to firebase

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Button } from 'react-native';
 var FBLoginButton = require('../FBLoginButton');
-import { LoginButton } from 'react-native-fbsdk';
-
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyDGHv1ykuKaLok2NjTYSZQmmf-mmnqmuO4",
+    authDomain: "chat-fa621.firebaseapp.com",
+    databaseURL: "https://chat-fa621.firebaseio.com",
+    projectId: "chat-fa621",
+    storageBucket: "chat-fa621.appspot.com",
+    messagingSenderId: "567933976367"
+};
+firebase.initializeApp(config);
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -10,8 +19,13 @@ export default class App extends Component<Props> {
     static navigationOptions = {
         header: null
     }
+    
+    onClick = () => {
+        console.log('CLick')
+    }
 
     render() {
+        console.log('vao render')
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>Login With FaceBook</Text>
@@ -21,19 +35,31 @@ export default class App extends Component<Props> {
                 <LoginButton
                     readPermissions={["email", "user_photos"]}
                     onLoginFinished={
-                        (error, result) => {
+                        async (error, result) => {
                             if (error) {
                                 alert("Login failed with error: " + error.message);
                             } else if (result.isCancelled) {
                                 alert("Login was cancelled");
                             } else {
                                 // alert("Login was successful with permissions: " + result.grantedPermissions)
+                                const tokenData = await AccessToken.getCurrentAccessToken();
+                                const token = tokenData.accessToken.toString();
+                                const credential = firebase.auth.FacebookAuthProvider.credential(token);
+                                const user = await firebase.auth().signInWithCredential(credential);
+                                console.log('user', user)
+                                firebase.database().ref(`/users/${user.uid}/profile`).set({
+                                    name: user.displayName,
+                                    email: user.email,
+                                    avatar: user.photoURL
+                                });
                                 this.props.navigation.navigate('dashboard')
+
                             }
                         }
                     }
                     onLogoutFinished={() => alert("User logged out")} />
                 <Button title="Go dashboard" onPress={() => this.props.navigation.navigate('dashboard')}></Button>
+                <Button title="Click" onPress={this.onClick}></Button>
             </View>
         );
     }
